32f421c55e2973bf7a1bcc4ef69e8142ae45fb7e
dev <jad@woopra.com>
2020-10-22 13:24:46 -0700

    debug

--- src/main/java/com/balanko/TestFrame.java
+++ src/main/java/com/balanko/TestFrame.java
@@ -8,6 +8,11 @@ package com.balanko;
 import java.net.InetAddress;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import net.java.games.input.Component;
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
+import net.java.games.input.Event;
+import net.java.games.input.EventQueue;
 
 /**
  *
@@ -53,6 +58,27 @@ public class TestFrame {
             } finally {
             }
         });
+
+        /* Create an event object for the underlying plugin to populate */
+        Event event = new Event();
+
+        /* Get the available controllers */
+        Controller[] controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
+        for (int i = 0; i < controllers.length; i++) {
+            /* Remember to poll each one */
+            controllers[i].poll();
+
+            /* Get the controllers event queue */
+            EventQueue queue = controllers[i].getEventQueue();
+
+            /* For each object in the queue */
+            while (queue.getNextEvent(event)) {
+                /* Get event component */
+                Component comp = event.getComponent();
+
+                System.err.println(comp);
+            }
+        }
 
 //           new Thread() {
 //            @Override
