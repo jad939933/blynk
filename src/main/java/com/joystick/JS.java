ed4df6f87568715a0832da629146a9cb37cb716e
jad y <dev@localhost-live.lan>
2020-11-04 18:32:49 +0200

    debug

--- src/main/java/com/joystick/JS.java
+++ src/main/java/com/joystick/JS.java
@@ -19,13 +19,13 @@ import net.java.games.input.EventQueue;
  * @author dev
  */
 public class JS {
-
+    
     private static XBeeDevice uart;
-
+    
     private static XBee64BitAddress addr_64;
-
+    
     public static void main(String[] args) throws Exception {
-
+        
         for (int i = 0; i < args.length; i++) {
             String p = args[i];
             int c = p.indexOf("=");
@@ -38,112 +38,97 @@ public class JS {
                 }
             }
         }
-
-        addr_64 = new XBee64BitAddress("00 13 A2 00 40 30 21 BA".replace(" ", ""));
-
-        uart = new XBeeDevice(System.getProperty("xbee.port"), 9600);
+        
+        addr_64 = new XBee64BitAddress(System.getProperty("xbee.dest_addr_64"));
+        
+        uart = new XBeeDevice(System.getProperty("xbee.port"), Integer.parseInt(System.getProperty("xbee.baud_rate")));
         uart.open();
-
+        
         System.out.println("UART open");
-
+        
         uart.sendData(new RemoteXBeeDevice(uart, addr_64), "|ACC 0 1000|ACC 1 1000|".getBytes());
-
+        
         Controller[] controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
-
+        
         Controller joystick = null;
-
+        
         for (int i = 0; i < controllers.length; i++) {
             Controller c = controllers[i];
             System.err.println(c.getName() + "...");
-            if (c.getName().equalsIgnoreCase("Sony Computer Entertainment Wireless Controller")) {
+            if (c.getName().equalsIgnoreCase(System.getProperty("joystick.name"))) {
                 joystick = c;
                 System.err.println(c.getName());
             }
         }
-
+        
         if (joystick == null) {
             System.out.println("Found no controllers.");
             System.exit(0);
         }
-
-        float x[] = {0}, y[] = {0};
-
-        new Thread() {
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                        send(x[0], y[0]);
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                    try {
-                        Thread.sleep(20);
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            }
-        }.start();
-
+        
+        
         while (true) {
-
+            
             joystick.poll();
-
+            
             EventQueue queue = joystick.getEventQueue();
-
+            
             Event event = new Event();
-
+            
+            float x=0, y=1;
+            
             while (queue.getNextEvent(event)) {
-
+                
                 Component comp = event.getComponent();
                 float value = event.getValue();
-
+                
                 switch (comp.getName().toLowerCase()) {
                     case "x":
-                        x[0] = value;
+                        x = value;
                         break;
                     case "y":
-                        y[0] = value;
+                        y = value;
                         break;
                 }
-
+                
             }
-
+            
+            send(x, y);
+            
             try {
-                Thread.sleep(20);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
-
+        
     }
-
+    
     static int lastX = 0, lastY = 0;
-
+    
     public static void send(float x, float y) throws Exception {
-
+        
         int _x = (int) (x * 512);
         int _y = (int) (y * 512);
-
+        
         if (lastX == _x && lastY == _y) {
             //skip
             return;
         }
-
+        
         lastX = _x;
         lastY = _y;
-
+        
         System.err.println(">>" + _x + ":" + _y);
-
+        
         String DATA_TO_SEND = "";
         if (_x == 0) {
             DATA_TO_SEND += "|STP 0|";
         } else {
             DATA_TO_SEND += "|SPD 0 " + Math.max(200, (int) (8 * Math.abs(_x))) + "|MV 0 " + ((_x > 0) ? "99999" : "-99999") + "|";
         }
-
+        
         if (_y == 0) {
             DATA_TO_SEND += "|STP 1|";
         } else {
