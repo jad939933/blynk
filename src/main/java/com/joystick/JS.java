c0050c0f6e10f46b8549b376ae8594a086abd652
jad y <dev@localhost-live.lan>
2020-11-04 18:40:39 +0200

    debug

--- src/main/java/com/joystick/JS.java
+++ src/main/java/com/joystick/JS.java
@@ -19,13 +19,13 @@ import net.java.games.input.EventQueue;
  * @author dev
  */
 public class JS {
-    
+
     private static XBeeDevice uart;
-    
+
     private static XBee64BitAddress addr_64;
-    
+
     public static void main(String[] args) throws Exception {
-        
+
         for (int i = 0; i < args.length; i++) {
             String p = args[i];
             int c = p.indexOf("=");
@@ -38,20 +38,20 @@ public class JS {
                 }
             }
         }
-        
+
         addr_64 = new XBee64BitAddress(System.getProperty("xbee.dest_addr_64"));
-        
+
         uart = new XBeeDevice(System.getProperty("xbee.port"), Integer.parseInt(System.getProperty("xbee.baud_rate")));
         uart.open();
-        
+
         System.out.println("UART open");
-        
+
         uart.sendData(new RemoteXBeeDevice(uart, addr_64), "|ACC 0 1000|ACC 1 1000|".getBytes());
-        
+
         Controller[] controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
-        
+
         Controller joystick = null;
-        
+
         for (int i = 0; i < controllers.length; i++) {
             Controller c = controllers[i];
             System.err.println(c.getName() + "...");
@@ -60,28 +60,27 @@ public class JS {
                 System.err.println(c.getName());
             }
         }
-        
+
         if (joystick == null) {
             System.out.println("Found no controllers.");
             System.exit(0);
         }
-        
-        
+
         while (true) {
-            
+
             joystick.poll();
-            
+
             EventQueue queue = joystick.getEventQueue();
-            
+
             Event event = new Event();
-            
-            float x=0, y=1;
-            
+
+            float x = 0, y = 1;
+
             while (queue.getNextEvent(event)) {
-                
+
                 Component comp = event.getComponent();
                 float value = event.getValue();
-                
+
                 switch (comp.getName().toLowerCase()) {
                     case "x":
                         x = value;
@@ -90,11 +89,11 @@ public class JS {
                         y = value;
                         break;
                 }
-                
+
             }
-            
+
             send(x, y);
-            
+
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
@@ -102,33 +101,29 @@ public class JS {
                 e.printStackTrace();
             }
         }
-        
+
     }
-    
-    static int lastX = 0, lastY = 0;
-    
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @throws Exception
+     */
     public static void send(float x, float y) throws Exception {
-        
+
         int _x = (int) (x * 512);
         int _y = (int) (y * 512);
-        
-        if (lastX == _x && lastY == _y) {
-            //skip
-            return;
-        }
-        
-        lastX = _x;
-        lastY = _y;
-        
+
         System.err.println(">>" + _x + ":" + _y);
-        
+
         String DATA_TO_SEND = "";
         if (_x == 0) {
             DATA_TO_SEND += "|STP 0|";
         } else {
             DATA_TO_SEND += "|SPD 0 " + Math.max(200, (int) (8 * Math.abs(_x))) + "|MV 0 " + ((_x > 0) ? "99999" : "-99999") + "|";
         }
-        
+
         if (_y == 0) {
             DATA_TO_SEND += "|STP 1|";
         } else {
